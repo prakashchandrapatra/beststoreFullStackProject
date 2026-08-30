a325250c6b24047bce14eb2b00737fe362435981
Prakash Chandra Patra <prakashchandrapatra285@example.com>
2026-08-30 13:46:34 +0530

    Remove broken Snippet.java and fix pom.xml dependencies

--- src/main/java/com/example/beststore/service/VideoService.java
+++ src/main/java/com/example/beststore/service/VideoService.java
@@ -1,11 +1,7 @@
-
-
-
 package com.example.beststore.service;
 
 import java.io.IOException;
 import java.util.List;
-
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
--- src/main/java/snippet/Snippet.java
+++
@@ -1,8 +0,0 @@
-package snippet;
-
-public class Snippet {
-	public static void main(String[] args) {
-		acela.proxy.rlwy.netacela.proxy.rlwy.netacela.proxy.rlwy.net470934709347093
-	}
-}
-
